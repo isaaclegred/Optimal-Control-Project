be6b900ab893c9eef716540d5ddcfc4dd3410dd8
Isaac Norman Legred <inl7@cornell.edu>
2019-12-13 11:53:28 -0500

    last video

--- MATLAB/Scratch.m
+++ MATLAB/Scratch.m
@@ -1,8 +1,8 @@
 close all
-[x, y] = meshgrid(linspace(-2,2,35), linspace(-2,2,35));
+[x, y] = meshgrid(linspace(-3,3,50), linspace(-3,3,50));
 coords = [x(:) y(:)];
 Adj = make_graph(coords,.15);
-[values, Frames] = Value(coords, @App_Cost,613, Adj, 100);
+[values, Frames] = Value(coords, @App_Cost,1275, Adj, 100);
 
 function C = Cost(current, targets)
 vdt = targets-current;
@@ -11,7 +11,7 @@ C =  sum(current.^2,2)*.4*((vdt(2)-v_0(2)*cos(current(2))).^2 +  (vdt(1) - v_0(1
 end
 function C_1  = App_Cost(current, targets)
 %v = [cos(current(1)); sin(current(2))];
-v= [cos(2*current(1)); sin(2*current(2))];
+v= [cos(current(2)); sin(current(1))];
 fdt = targets - current;
 norms  = sqrt(sum(fdt.^2,2));
 a = fdt./([norms, norms]);
@@ -87,8 +87,8 @@ while(counter < iters)
                 current_direction = [i, try_direction(1)];
             end
         end
-        if mod(node, 600)==0
-            contourf(linspace(-2,2,35), linspace(-2,2,35), reshape(Values, [35,35]), 15 );
+        if mod(node, 2000)==0
+            contourf(linspace(-3,3,50), linspace(-3,3,50), reshape(Values, [50,50]), 22 );
             F = getframe;
             Frames = [Frames, F];
         end
